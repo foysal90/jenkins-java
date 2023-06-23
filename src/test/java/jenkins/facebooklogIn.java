35845bcc3afb0889b31e5d6c2d08fa91ef6cabcc
foysal90 <foysal1416@hotmail.com>
2023-06-22 22:26:46 -0400

    second commit

--- src/test/java/jenkins/facebooklogIn.java
+++ src/test/java/jenkins/facebooklogIn.java
@@ -3,16 +3,25 @@ package jenkins;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class facebooklogIn {
 
     public static WebDriver driver;
-
+@Parameters("Browser")
     @Test
-    public  void fbLogin(){
+    public  void fbLogin(String browserName){
 
+    if (browserName.contains("Chrome")){
         driver = new ChromeDriver();
+    }
+    else{
+        driver = new SafariDriver();
+    }
+
+
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
         driver.findElement(By.xpath("//input[@id='email']")).sendKeys("foy21");
